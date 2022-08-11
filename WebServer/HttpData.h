f2b94745a05d41ed4c73cbd5858b1fc6931fc3e4
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-11 17:13:44 +0800

    Update HttpData.h

--- WebServer/HttpData.h
+++ WebServer/HttpData.h
@@ -49,22 +49,20 @@ enum ParseState {
   H_END_LF
 };
 
+
 enum ConnectionState { H_CONNECTED = 0, H_DISCONNECTING, H_DISCONNECTED };
-
 enum HttpMethod { METHOD_POST = 1, METHOD_GET, METHOD_HEAD };
-
 enum HttpVersion { HTTP_10 = 1, HTTP_11 };
 
 class MimeType {
  private:
   static void init();
   static std::unordered_map<std::string, std::string> mime;
+  
   MimeType();
   MimeType(const MimeType &m);
-
  public:
   static std::string getMime(const std::string &suffix);
-
  private:
   static pthread_once_t once_control;
 };
@@ -85,22 +83,28 @@ class HttpData : public std::enable_shared_from_this<HttpData> {
   void newEvent();
 
  private:
+  bool error_;
+  bool keepAlive_;
+  int fd_;
+  ConnectionState connectionState_;  
+  
   EventLoop *loop_;
   std::shared_ptr<Channel> channel_;
-  int fd_;
+  
+  
   std::string inBuffer_;
   std::string outBuffer_;
-  bool error_;
-  ConnectionState connectionState_;
+
 
   HttpMethod method_;
   HttpVersion HTTPVersion_;
   std::string fileName_;
-  std::string path_;
+  std::string path_
+    ;
   int nowReadPos_;
   ProcessState state_;
   ParseState hState_;
-  bool keepAlive_;
+
   std::map<std::string, std::string> headers_;
   std::weak_ptr<TimerNode> timer_;
 
