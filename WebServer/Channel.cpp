5d53fb8dba19c9c0b284c67dd1712193dc34d1e1
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-11 15:33:29 +0800

    Update Channel.cpp

--- WebServer/Channel.cpp
+++ WebServer/Channel.cpp
@@ -1,33 +1,26 @@
-// @Author Lin Ya
-// @Email xxbbb@vip.qq.com
 #include "Channel.h"
-
-#include <unistd.h>
-#include <cstdlib>
-#include <iostream>
-
-#include <queue>
-
 #include "Epoll.h"
 #include "EventLoop.h"
 #include "Util.h"
 
+
+#include <unistd.h>
+#include <cstdlib>
+#include <iostream>
+#include <queue>
+
+
+
 using namespace std;
-
-Channel::Channel(EventLoop *loop)
-    : loop_(loop), events_(0), lastEvents_(0), fd_(0) {}
-
-Channel::Channel(EventLoop *loop, int fd)
-    : loop_(loop), fd_(fd), events_(0), lastEvents_(0) {}
-
-Channel::~Channel() {
-  // loop_->poller_->epoll_del(fd, events_);
-  // close(fd_);
-}
+//
+Channel::Channel(EventLoop *loop): loop_(loop), events_(0), lastEvents_(0), fd_(0) {}
+Channel::Channel(EventLoop *loop, int fd): loop_(loop), fd_(fd), events_(0), lastEvents_(0) {}
+Channel::~Channel() {}
 
 int Channel::getFd() { return fd_; }
 void Channel::setFd(int fd) { fd_ = fd; }
 
+// 用函数封装一层
 void Channel::handleRead() {
   if (readHandler_) {
     readHandler_();
