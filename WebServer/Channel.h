d7b95fd2c818336605e7569d052ab7986ce47e58
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-11 15:33:18 +0800

    Update Channel.h

--- WebServer/Channel.h
+++ WebServer/Channel.h
@@ -1,5 +1,4 @@
-// @Author Lin Ya
-// @Email xxbbb@vip.qq.com
+
 #pragma once
 #include <sys/epoll.h>
 #include <sys/epoll.h>
@@ -15,6 +14,7 @@ class HttpData;
 class Channel {
  private:
   typedef std::function<void()> CallBack;
+ 
   EventLoop *loop_;
   int fd_;
   __uint32_t events_;
@@ -33,14 +33,22 @@ class Channel {
   CallBack writeHandler_;
   CallBack errorHandler_;
   CallBack connHandler_;
-
  public:
   Channel(EventLoop *loop);
   Channel(EventLoop *loop, int fd);
   ~Channel();
+ 
   int getFd();
   void setFd(int fd);
-
+  void setRevents(__uint32_t ev) { revents_ = ev; }
+  void setEvents(__uint32_t ev) { events_ = ev; }
+  __uint32_t &getEvents() { return events_; }
+  __uint32_t getLastEvents() { return lastEvents_; }
+  bool EqualAndUpdateLastEvents() {
+    bool ret = (lastEvents_ == events_);
+    lastEvents_ = events_;
+    return ret;
+  }
   void setHolder(std::shared_ptr<HttpData> holder) { holder_ = holder; }
   std::shared_ptr<HttpData> getHolder() {
     std::shared_ptr<HttpData> ret(holder_.lock());
@@ -48,14 +56,9 @@ class Channel {
   }
 
   void setReadHandler(CallBack &&readHandler) { readHandler_ = readHandler; }
-  void setWriteHandler(CallBack &&writeHandler) {
-    writeHandler_ = writeHandler;
-  }
-  void setErrorHandler(CallBack &&errorHandler) {
-    errorHandler_ = errorHandler;
-  }
+  void setWriteHandler(CallBack &&writeHandler) { writeHandler_ = writeHandler; }
+  void setErrorHandler(CallBack &&errorHandler) { errorHandler_ = errorHandler; }
   void setConnHandler(CallBack &&connHandler) { connHandler_ = connHandler; }
-
   void handleEvents() {
     events_ = 0;
     if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
@@ -75,23 +78,13 @@ class Channel {
     }
     handleConn();
   }
+ 
   void handleRead();
   void handleWrite();
   void handleError(int fd, int err_num, std::string short_msg);
   void handleConn();
+ 
 
-  void setRevents(__uint32_t ev) { revents_ = ev; }
-
-  void setEvents(__uint32_t ev) { events_ = ev; }
-  __uint32_t &getEvents() { return events_; }
-
-  bool EqualAndUpdateLastEvents() {
-    bool ret = (lastEvents_ == events_);
-    lastEvents_ = events_;
-    return ret;
-  }
-
-  __uint32_t getLastEvents() { return lastEvents_; }
 };
 
 typedef std::shared_ptr<Channel> SP_Channel;
