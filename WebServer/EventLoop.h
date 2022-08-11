63b3d499bc17a0077cd2a19376d3d0dfbbd4a689
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-11 16:12:24 +0800

    Update EventLoop.h

--- WebServer/EventLoop.h
+++ WebServer/EventLoop.h
@@ -1,5 +1,3 @@
-// @Author Lin Ya
-// @Email xxbbb@vip.qq.com
 #pragma once
 #include <functional>
 #include <memory>
@@ -20,15 +18,18 @@ class EventLoop {
   typedef std::function<void()> Functor;
   EventLoop();
   ~EventLoop();
+ 
   void loop();
   void quit();
-  void runInLoop(Functor&& cb);
-  void queueInLoop(Functor&& cb);
+
+  // ***重点
   bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
   void assertInLoopThread() { assert(isInLoopThread()); }
-  void shutdown(shared_ptr<Channel> channel) { shutDownWR(channel->getFd()); }
+ 
+  void runInLoop(Functor&& cb); // 
+  void queueInLoop(Functor&& cb);// 
+ 
   void removeFromPoller(shared_ptr<Channel> channel) {
-    // shutDownWR(channel->getFd());
     poller_->epoll_del(channel);
   }
   void updatePoller(shared_ptr<Channel> channel, int timeout = 0) {
@@ -37,19 +38,25 @@ class EventLoop {
   void addToPoller(shared_ptr<Channel> channel, int timeout = 0) {
     poller_->epoll_add(channel, timeout);
   }
+ 
+  void shutdown(shared_ptr<Channel> channel) { shutDownWR(channel->getFd()); }
 
  private:
   // 声明顺序 wakeupFd_ > pwakeupChannel_
   bool looping_;
-  shared_ptr<Epoll> poller_;
-  int wakeupFd_;
   bool quit_;
   bool eventHandling_;
+ 
+  shared_ptr<Epoll> poller_;
+  int wakeupFd_;
+  shared_ptr<Channel> pwakeupChannel_;
+
   mutable MutexLock mutex_;
   std::vector<Functor> pendingFunctors_;
   bool callingPendingFunctors_;
+ 
   const pid_t threadId_;
-  shared_ptr<Channel> pwakeupChannel_;
+
 
   void wakeup();
   void handleRead();
