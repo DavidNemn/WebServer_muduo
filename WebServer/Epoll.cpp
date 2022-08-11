a468d440e526ae280068c4545deba2ff4d39be8b
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-11 15:25:03 +0800

    Update Epoll.cpp

--- WebServer/Epoll.cpp
+++ WebServer/Epoll.cpp
@@ -22,23 +22,59 @@ const int EPOLLWAIT_TIME = 10000;
 
 typedef shared_ptr<Channel> SP_Channel;
 
-Epoll::Epoll() : epollFd_(epoll_create1(EPOLL_CLOEXEC)), events_(EVENTSNUM) {
-  assert(epollFd_ > 0);
-}
+Epoll::Epoll() : epollFd_(epoll_create1(EPOLL_CLOEXEC)), events_(EVENTSNUM) { assert(epollFd_ > 0); }
 Epoll::~Epoll() {}
 
+
+// 返回活跃事件数
+std::vector<SP_Channel> Epoll::poll() {
+  while (true) {
+    int event_count = epoll_wait(epollFd_, &*events_.begin(), events_.size(), EPOLLWAIT_TIME);
+    if (event_count < 0) perror("epoll wait error");
+    std::vector<SP_Channel> req_data = getEventsRequest(event_count);
+    if (req_data.size() > 0) return req_data;
+  }
+}
+
+
+// 分发处理函数
+std::vector<SP_Channel> Epoll::getEventsRequest(int events_num) {
+  std::vector<SP_Channel> req_data;
+  for (int i = 0; i < events_num; ++i) {
+    // 获取有事件产生的描述符
+    int fd = events_[i].data.fd;
+    SP_Channel cur_req = fd2chan_[fd];
+    if (cur_req) {
+      // 通过活跃fd找到channel
+      cur_req->setRevents(events_[i].events);
+      cur_req->setEvents(0);
+      // 加入线程池之前将Timer和request分离
+      // cur_req->seperateTimer();
+      req_data.push_back(cur_req);
+    } else {
+      LOG << "SP cur_req is invalid";
+    }
+  }
+  return req_data;
+}
+
+
 // 注册新描述符
+// 将Channel加到当前的epoll上
+//    定时器处理
+//    fd2http_ + fd2chan_ （分开的原因）
+//    epoll_ctl
 void Epoll::epoll_add(SP_Channel request, int timeout) {
   int fd = request->getFd();
   if (timeout > 0) {
     add_timer(request, timeout);
     fd2http_[fd] = request->getHolder();
   }
+  
   struct epoll_event event;
   event.data.fd = fd;
   event.events = request->getEvents();
-
-  request->EqualAndUpdateLastEvents();
+  request->EqualAndUpdateLastEvents(); // 为了epoll_mod方便
 
   fd2chan_[fd] = request;
   if (epoll_ctl(epollFd_, EPOLL_CTL_ADD, fd, &event) < 0) {
@@ -48,6 +84,8 @@ void Epoll::epoll_add(SP_Channel request, int timeout) {
 }
 
 // 修改描述符状态
+//  定时器处理
+//  epoll_ctl
 void Epoll::epoll_mod(SP_Channel request, int timeout) {
   if (timeout > 0) add_timer(request, timeout);
   int fd = request->getFd();
@@ -63,54 +101,19 @@ void Epoll::epoll_mod(SP_Channel request, int timeout) {
 }
 
 // 从epoll中删除描述符
+//  epoll_ctl
+//  fd2chan_ + fd2http_
 void Epoll::epoll_del(SP_Channel request) {
   int fd = request->getFd();
   struct epoll_event event;
   event.data.fd = fd;
   event.events = request->getLastEvents();
-  // event.events = 0;
-  // request->EqualAndUpdateLastEvents()
-  if (epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &event) < 0) {
-    perror("epoll_del error");
-  }
+  if(epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &event) < 0) { perror("epoll_del error"); }
   fd2chan_[fd].reset();
   fd2http_[fd].reset();
 }
 
-// 返回活跃事件数
-std::vector<SP_Channel> Epoll::poll() {
-  while (true) {
-    int event_count =
-        epoll_wait(epollFd_, &*events_.begin(), events_.size(), EPOLLWAIT_TIME);
-    if (event_count < 0) perror("epoll wait error");
-    std::vector<SP_Channel> req_data = getEventsRequest(event_count);
-    if (req_data.size() > 0) return req_data;
-  }
-}
-
 void Epoll::handleExpired() { timerManager_.handleExpiredEvent(); }
-
-// 分发处理函数
-std::vector<SP_Channel> Epoll::getEventsRequest(int events_num) {
-  std::vector<SP_Channel> req_data;
-  for (int i = 0; i < events_num; ++i) {
-    // 获取有事件产生的描述符
-    int fd = events_[i].data.fd;
-
-    SP_Channel cur_req = fd2chan_[fd];
-
-    if (cur_req) {
-      cur_req->setRevents(events_[i].events);
-      cur_req->setEvents(0);
-      // 加入线程池之前将Timer和request分离
-      // cur_req->seperateTimer();
-      req_data.push_back(cur_req);
-    } else {
-      LOG << "SP cur_req is invalid";
-    }
-  }
-  return req_data;
-}
 
 void Epoll::add_timer(SP_Channel request_data, int timeout) {
   shared_ptr<HttpData> t = request_data->getHolder();
