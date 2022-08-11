f658f6b6fe56de3c7ae46f07277f9e9cdfb7a0e1
David <66623990+DavidNemn@users.noreply.github.com>
2022-08-11 15:58:55 +0800

    Update EventLoopThreadPool.h

--- WebServer/EventLoopThreadPool.h
+++ WebServer/EventLoopThreadPool.h
@@ -1,5 +1,3 @@
-// @Author Lin Ya
-// @Email xxbbb@vip.qq.com
 #pragma once
 #include <memory>
 #include <vector>
@@ -7,21 +5,23 @@
 #include "base/Logging.h"
 #include "base/noncopyable.h"
 
-
+// 所有的reactor
+//  EventLoopThread封装thread + EventLoop 
+//  thread封装真实的thread
 class EventLoopThreadPool : noncopyable {
  public:
   EventLoopThreadPool(EventLoop* baseLoop, int numThreads);
-
   ~EventLoopThreadPool() { LOG << "~EventLoopThreadPool()"; }
+  
   void start();
-
   EventLoop* getNextLoop();
 
  private:
-  EventLoop* baseLoop_;
   bool started_;
   int numThreads_;
   int next_;
+ 
+  EventLoop* baseLoop_;
   std::vector<std::shared_ptr<EventLoopThread>> threads_;
   std::vector<EventLoop*> loops_;
 };
